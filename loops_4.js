cd8408716d8bd0fc2e0e6636ec8a6289391f86b9
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-25 23:00:21 +0530

    loops_ex

---
+++ loops_4.js
@@ -0,0 +1,6 @@
+// Print numbers from 10 to 1 (reverse order).
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+    
+}
---
+++ loops_5.js
@@ -0,0 +1,8 @@
+// Print the multiplication table of any number.
+
+let num = 4;
+
+for (let i = 1; i <= 10; i++){
+    console.log(num+" X "+i+" = "+num*i);
+    
+}
